344d634dbca96dd82aca7caf9fd2b9bcb623566b
Amit Shinde <AmitS13@citiustech.com>
2021-12-20 13:59:47 +0530

    angular employee example added 2 pm

---
+++ AngularTasks/emp-project/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  
+  public eid:number=23456234;
+  public ename:string="Stephen";
+  public job:string="Manager";
+  public salary:number=75000;
+  public dept:number=10;
+  
+}
